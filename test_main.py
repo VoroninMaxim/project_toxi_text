7be4e8f25c3c43e6454ec17644aa24b7d6e0aa83
VoroninMaxim <maksimsazonov476@gmail.com>
2023-12-25 19:15:36 +0300

    tests-1

--- test_main.py
+++ test_main.py
@@ -24,22 +24,22 @@ from streamlit.testing.v1 import AppTest
 
 def test_project_main():
     at = AppTest.from_file("projec_main.py")
-    at.secrets['db_username'] = 'myuser'
-    at.run()
+    #at.secrets['db_username'] = 'myuser'
+    #at.run()
     # at.number_input[0].increment().run()
     # at.button[0].click().run()
     # assert at.markdown[0].value <= 0.01
     #assert AppTest.from_file("projec_main.py").run()
-    assert not at.exception
+    #assert not at.exception
 
-    at.text_input('db_username').input('VoroninMaxim').run()
+    #at.text_input('db_username').input('VoroninMaxim').run()
     #assert at.warning[0].value == 'Try again.'
 
     #assert at.button[0].value == True
     # at.button[0].click().run()
     # assert at.button[0].value == True
 
-    #assert "My Text Toxicity App" in at.title[0].value
+    assert "My Text Toxicity App" in at.title[0].value
 
 
 ####----https://docs.streamlit.io/library/advanced-features/app-testing/cheat-sheet
